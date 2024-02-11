a1f551e57831879ca6a906ba2ed75669b9811068
NitroGuy10 <naviyer10@gmail.com>
2024-02-11 04:39:54 -0600

    made tha game!!

--- components/GameWindow.tsx
+++ components/GameWindow.tsx
@@ -2,39 +2,113 @@ import React from "react";
 import { P5CanvasInstance, SketchProps, type Sketch } from "@p5-wrapper/react";
 import { NextReactP5Wrapper } from "@p5-wrapper/next";
 
-type MySketchProps = SketchProps & {
-    rotation: number;
+type ComponentProps = {
+    letterToSign: string;
+    success: boolean;
 };
+
+type MySketchProps = SketchProps & ComponentProps;
   
 function sketch(p5: P5CanvasInstance<MySketchProps>) {
 
-  p5.setup = () => p5.createCanvas(800, 500);
+  p5.setup = () => p5.createCanvas(1000, 600);
+  p5.frameRate(60);
 
-  let rotation = 0;
+  let letterToSign = "a";
+  let letterX = 0;
+  let letterY = 0;
+  let letterXVelocity = 5;
+  let letterYVelocity = 5;
+  let letterRotation = 0;
+  let letterRotationVelocity = 0;
+  let shattered = false;
+
+  let fragmentX = 0;
+    let fragmentY = 0;
+    let fragmentXVelocity = 0;
+    let fragmentYVelocity = 0;
+    let fragmentRotation = 0;
+    let fragmentRotationVelocity = 0;
 
   p5.updateWithProps = (props: MySketchProps) => {
-    if (props.rotation) {
-      rotation = (props.rotation * Math.PI) / 180;
+    if (props.letterToSign && letterToSign !== props.letterToSign) {
+        letterToSign = props.letterToSign;
+        let newXVelocity = p5.random(-5, 5);
+        letterX = (p5.width / 2) - (newXVelocity * p5.width / 9);
+        // letterX = p5.width / 2;
+        letterY = p5.height;
+        letterXVelocity = newXVelocity;
+        letterYVelocity = -8.8;
+        letterRotation = 0;
+        letterRotationVelocity = p5.random(-0.001, 0.001);
+    }
+    if (props.success !== undefined) {
+        shattered = props.success;
+
+        if (shattered) {
+            fragmentX = letterX;
+            fragmentY = letterY;
+            fragmentXVelocity = letterXVelocity + p5.random(-3, 3);
+            fragmentYVelocity = letterYVelocity + p5.random(-2, 2);
+            fragmentRotation = letterRotation;
+            fragmentRotationVelocity = letterRotationVelocity + p5.random(-0.01, 0.01);
+
+            letterXVelocity += p5.random(-3, 3);
+            letterYVelocity += p5.random(-2, 2);
+            letterRotationVelocity += p5.random(-0.01, 0.01);
+        }
     }
   };
 
   p5.draw = () => {
-    p5.background(0);
+    letterX += letterXVelocity;
+    letterY += letterYVelocity;
+    letterYVelocity += 0.07;
+    letterRotation += letterRotationVelocity;
+    if (shattered)
+    {
+        fragmentX += fragmentXVelocity;
+        fragmentY += fragmentYVelocity;
+        fragmentYVelocity += 0.07;
+        fragmentRotation += fragmentRotationVelocity;
+    }
+
+    p5.background(100);
+    if (shattered) {
+        p5.background(255, 0, 0);
+    }
+    // p5.push();
+    // p5.rectMode(p5.CENTER);
+    // p5.translate(p5.width / 2, p5.height / 2);
+    // p5.fill(255, 0, 0);
+    // p5.rect(0, 0, 50, 50);
+    // p5.fill(255);
+    // p5.pop();
+    
     p5.push();
-    p5.rectMode(p5.CENTER);
-    p5.translate(p5.width / 2, p5.height / 2);
-    p5.rotate(rotation);
-    p5.fill(255, 0, 0);
-    p5.rect(0, 0, 50, 50);
+    p5.textSize(92);
+    p5.textAlign(p5.CENTER, p5.CENTER);
+    p5.translate(letterX, letterY);
+    p5.rotate(letterRotation);
+    p5.text(letterToSign, 0, 0);
     p5.pop();
+
+    if (shattered)
+    {
+        p5.push();
+        p5.textSize(92);
+        p5.textAlign(p5.CENTER, p5.CENTER);
+        p5.translate(fragmentX, fragmentY);
+        p5.rotate(fragmentRotation);
+        p5.text(letterToSign, 0, 0);
+        p5.pop();
+    }
   };
 };
 
-type ComponentProps = {
-    rotation: number;
-};
+
 
 export default function Page(props: ComponentProps) {
 
-  return <NextReactP5Wrapper sketch={sketch} rotation={props.rotation} />;
+  return <NextReactP5Wrapper sketch={sketch} letterToSign={props.letterToSign} success={props.success} />;
 }
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,23 +1,54 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import GameWindow from "@/components/gameWindow";
-import { useState } from "react";
+import GameWindow from "@/components/GameWindow";
+import { use, useEffect, useState } from "react";
+import random, { RNG } from "random";
+import seedrandom from "seedrandom";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Game() {
-  const [rotation, setRotation] = useState(0);
+random.use(seedrandom("coSign") as unknown as RNG);
 
-  return (
+export default function Game() {
+    const [timeLeft, setTimeLeft] = useState(60);
+    const [letterToSign, setLetterToSign] = useState("a");
+    const [success, setSuccess] = useState(false);
+
+    
+    useEffect(() => {        
+        if (!timeLeft) return;
+
+        if (timeLeft % 5 === 0) {
+            let randomLetter = letterToSign;
+            setSuccess(false);
+            while (randomLetter === letterToSign) {
+                randomLetter = String.fromCharCode(65 + random.int(0, 25))
+            }
+            setLetterToSign(randomLetter);
+
+            setTimeout(() => {
+                // store the letter data in firebase
+
+            }, 4900);
+        }
+
+        const intervalId = setInterval(() => {
+            setTimeLeft(timeLeft - 1);
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [timeLeft]);
+
+
+    return (
     <div>
-      <p className="text-5xl font-inter text-center">coSign</p>
-      <div className="block mx-auto">
-        <GameWindow rotation={rotation} />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setRotation(rotation + 10)}
-        ></button>
-      </div>
+        <p className="text-5xl font-inter text-center">coSign</p>
+        <div className="block mx-auto">
+            <GameWindow letterToSign={letterToSign} success={success} />
+        </div>
+        <p className="text-3xl font-bold text-center">{timeLeft}</p>
+        <button onClick={() => setSuccess(true)} className="text-3xl font-bold text-center">{success ? "yay!" : "SHATTER"}</button>
+        <p>{letterToSign}</p>
     </div>
-  );
+    );
 }
