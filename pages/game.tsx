7371bf68a8be50a26da86b8db4b42cd023df437d
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2024-02-11 09:19:59 -0600

    adding bracket

--- pages/game.tsx
+++ pages/game.tsx
@@ -139,6 +139,7 @@ export default function Game() {
                             score : score
                         })
                     }
+                    }  
             }, 6500);
         }
 
