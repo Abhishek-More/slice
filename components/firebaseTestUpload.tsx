649fd37e2f7e1d4539e0948406683638030eb1d7
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2024-02-11 04:26:39 -0600

    Added base updatePlayer

--- components/firebaseTestUpload.tsx
+++ components/firebaseTestUpload.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getFirestore, collection, getDocs, doc, onSnapshot, setDoc } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
+import { string } from 'prop-types';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -19,24 +20,39 @@ const firestore = getFirestore(app);
 export default function FirebaseTestUpload() {
     const [val, setVal] = useState<string>();
     const [name, setName] = useState<string>();
+    const [player, setPlayer] = useState<string>();
+    
 
     const addData = async () => {
         try {
-        await setDoc(doc(firestore, "test", "yes"), {
-            name: name,
-            value: val,
-        });
-        console.log('Document successfully written!');
+            await setDoc(doc(firestore, "test", "yes"), {
+                name: name,
+                value: val,
+            });
+            console.log('Document successfully written!');
         } catch (error) {
-        console.error('Error writing document: ', error);
+            console.error('Error writing document: ', error);
         }
     };
+
+    const updatePlayer = async () => {
+        try {
+            await setDoc(doc(firestore, "players", player!), {
+                confidences : confidences,
+                correct : correct,
+                incorrect : incorrect,
+                letters : letters,
+                score : score,
+            })
+        }
+    }
     
     return (
       <div>
         <h1>Upload!</h1>
         <input type="text" onChange={(e) => setName(e.target.value)}></input>
         <input type="text" onChange={(e) => setVal(e.target.value)}></input>
+        <input type="text" onChange={(e) => setPlayer(e.target.value)}></input>
         <button onClick={addData}>Upload Data</button>
       </div>
     );
