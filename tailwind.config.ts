f7a31f50c1270c77cb9671d2f147e27ac09cb596
Li Shen <lililogan@gmail.com>
2024-02-11 10:06:55 -0600

    landing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,12 @@ const config: Config = {
   theme: {
     colors: {
       'background_green': '#252927',
+      'norm_white': '#FFFFFF',
+      'pale_yellow': '#FFF9D4',
     },
     extend: {
       backgroundImage: {
+        'gradient-linear': 'linear-gradient(to right, rgb(134, 239, 172), rgb(253, 224, 71), rgb(249, 168, 212), var(--tw-gradient-stops))',
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
